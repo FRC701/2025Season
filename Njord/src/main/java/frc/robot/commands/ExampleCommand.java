e08f0e20caa94c31b6f8337e5b5e7a208973e24a
Jesse Melendres <jessejacobm@vandenrobotics.com>
2025-01-11 11:10:42 -0800

    Spotless Apply testing
    not finished!

--- Njord/src/main/java/frc/robot/commands/ExampleCommand.java
+++ Njord/src/main/java/frc/robot/commands/ExampleCommand.java
@@ -6,12 +6,19 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-
+import frc.robot.subsystems.ExampleSubsystem;
 /** An example command that uses an example subsystem. */
 public class ExampleCommand extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final ExampleSubsystem m_subsystem;
+  private ExampleSubsystem mMotor;
+  private double mSpeed;
 
+  public ExampleCommand(ExampleSubsystem ExampleSubsystem, double speed)
+  this.ExampleCommand = ExampleCommand;
+  this.mMotor = ExampleCommand;
+  this.mSpeed = speed;
+  }
   /**
    * Creates a new ExampleCommand.
    *
@@ -29,15 +36,20 @@ public class ExampleCommand extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    mMotor.spinExampleCommand();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    mMotor.stopExampleCommand();
+    
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
   }
-}
+} 
